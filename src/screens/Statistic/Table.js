a6ffd96b604e785d8181ad2ac6bcfef681608000
rifqi3004 <ti17.0127@gmail.com>
2019-01-12 11:25:22 +0700

    table

--- src/screens/Statistic/Table.js
+++ src/screens/Statistic/Table.js
@@ -1,41 +1,38 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { Container, Row, Col, Text, Content, Grid } from 'native-base';
+import { View, StyleSheet, WebView } from 'react-native';
+import { Container, Row, Col, Text, Content, Grid, Spinner } from 'native-base';
 
 class Table extends React.Component{
-  data =[1,2,3,4]
-  renderRow(){
-    return(
-      <View style={styles.row}>
-        <View style={styles.col}/>
-        <View style={styles.col}/>
-        <View style={styles.col}/>
-        <View style={styles.col}/>
-      </View>
-    );
+  renderLoading(){
+    return(<Spinner />)
   }
-
   render(){
     return(
-      <Container>
-        {/* <Content>
-          <Grid>
-            <Row>
-              <Col><Text>Time</Text></Col>
-              <Col><Text>Water pH</Text></Col>
-              <Col><Text>Temperatur</Text></Col>
-              <Col><Text>Height</Text></Col>
-            </Row>
-          </Grid>
-        </Content> */}
-        <View style={styles.table}>
-          {
-            this.data.map(()=> {
-                return this.renderRow();
-            })
-          }
-        </View>
-      </Container>
+      <WebView
+      source={{uri: 'http://139.180.220.65:3333/table'}}
+      style={{marginTop: 20}}
+      renderLoading={this.renderLoading}
+    startInLoadingState
+    />
+      // <Container>
+      //   {/* <Content>
+      //     <Grid>
+      //       <Row>
+      //         <Col><Text>Time</Text></Col>
+      //         <Col><Text>Water pH</Text></Col>
+      //         <Col><Text>Temperatur</Text></Col>
+      //         <Col><Text>Height</Text></Col>
+      //       </Row>
+      //     </Grid>
+      //   </Content> */}
+      //   <View style={styles.table}>
+      //     {
+      //       this.data.map(()=> {
+      //           return this.renderRow();
+      //       })
+      //     }
+      //   </View>
+      // </Container>
     )
   }
 }
